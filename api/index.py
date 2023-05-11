62b6f645c14286ca43e0e89599433357a7e99b01
liangxu01 <liangxu@umass.edu>
2023-05-11 16:41:07 -0400

    added water

--- api/index.py
+++ api/index.py
@@ -24,7 +24,7 @@ def cordinates(cords):
     report = createReport(lat, lon)
     return report
 
-@app.route('/user=<string:user>/add=<integer:water>')
+@app.route('/user=<string:user>/add=<int:water>')
 def user(user, water):
     report = {'user': user, 'liters': water}
     return json.dumps(report)
