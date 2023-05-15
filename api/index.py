530b07ff62dbdb1b34c031a16144ed1adf6a20d4
liangxu01 <liangxu@umass.edu>
2023-05-15 19:05:01 -0400

    added daily updated

--- api/index.py
+++ api/index.py
@@ -42,4 +42,20 @@ def user(user, water):
     return json.dumps(report)
 
 
+@app.route('/user=<string:user>/add_daily=<int:water>')
+def user(user, water):
+
+    #Access the SUPABASE client
+    url = "https://vrugylomdozzwscsaelr.supabase.co"
+    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZydWd5bG9tZG96endzY3NhZWxyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MzcyNTU1NiwiZXhwIjoxOTk5MzAxNTU2fQ.8Fiu6YZtZBX1ZSZ-N84Q3LZvK3ukzfjJC0lERR_JOHI"
+    supabase: Client = create_client(url, key)
+
+    if water != 0:
+        addLitters(supabase, user, water)
+
+    curWater = getLitters(supabase, user)
+    report = {'user': user, 'liters': curWater}
+
+    return json.dumps(report)
+
 
